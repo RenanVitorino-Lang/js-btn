2da62b4f227d5192dd9af9ce12eb43decacab4a7
RenanVitorino-Lang <renandapaz10@gmail.com>
2024-09-28 11:47:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function solicitarDados() {
+    //Solicitar o cartão do usuario
+    let cartao = prompt("Qual numero do seu cartão?");
+    //Solicitar os 3 numeros de tras
+     let numero = prompt("Quais são os 3 numeruzinhos insignificantes de tras?");
+     // Exibe a saudação
+     alert("Obrigado pelo numero do cartão, " + cartao + " e o codigo " + numero );
+     //mostrar dados no console 
+     console.log("cartão do usuario", cartao)
+     console.log("3 numeros", numero)
+
+
+
+
+
+
+
+
+}
